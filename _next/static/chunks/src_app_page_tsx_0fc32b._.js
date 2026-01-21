92341bc00d1db501b8e4329b5decde28e6975b99
nexus <nexus@localhost.localdomain>
2026-01-21 15:23:11 -0300

    Updates

---
+++ _next/static/chunks/src_app_page_tsx_0fc32b._.js
@@ -0,0 +1,17 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_0fc32b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_0fc32b._.js",
+  "chunks": [
+    "static/chunks/src_bcca41._.js",
+    "static/chunks/node_modules_next_dist_64bad8._.js",
+    "static/chunks/node_modules_@supabase_realtime-js_dist_module_d96433._.js",
+    "static/chunks/node_modules_@supabase_storage-js_dist_index_mjs_1d76dc._.js",
+    "static/chunks/node_modules_@supabase_auth-js_dist_module_419227._.js",
+    "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854ac._.js",
+    "static/chunks/node_modules_c2dddf._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ _next/static/chunks/src_contexts_AdminAuthContext_tsx_ee3c7c._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_contexts_AdminAuthContext_tsx_ee3c7c._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_contexts_AdminAuthContext_tsx_ee3c7c._.js",
+  "chunks": [
+    "static/chunks/node_modules_c7a528._.js",
+    "static/chunks/src_contexts_AdminAuthContext_tsx_1ccc62._.js"
+  ],
+  "source": "dynamic"
+});
